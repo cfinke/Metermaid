9818109a1f6015ee29f2c2bac443e64e7c8e17e0
Christopher Finke <cfinke@gmail.com>
2023-07-09 04:14:59 +0000

    Basic skeleton plugin with menu and admin page.

--- metermaid.php
+++ metermaid.php
@@ -1,11 +1,51 @@
 <?php
 
 /*
-Plugin Name: Metermaid 
+Plugin Name: Metermaid
 Description: Assists in tracking water usage for community wells.
 Version: 1.0
 Author: Christopher Finke
 */
 
 class METERMAID {
+	public static function init() {
+		add_action( 'admin_menu', array( __CLASS__, 'add_options_menu' ) );
+	}
+
+	public static function add_options_menu() {
+		add_menu_page(
+			'Metermaid',
+			'Metermaid',
+			'publish_posts',
+			'metermaid',
+			array( 'METERMAID', 'admin_page' ),
+			'dashicons-welcome-write-blog',
+			4
+		);
+		/*
+		add_submenu_page(
+			'metermaid',
+			'Metermaid',
+			'Metermaid',
+			'publish_posts',
+			'metermaid',
+			array( 'METERMAID', 'admin_page' ),
+			1
+		);
+
+		remove_submenu_page( 'metermaid','metermaid' );
+		*/
+	}
+
+	public static function admin_page() {
+		global $wpdb;
+
+		?>
+		<div class="wrap">
+			<h2>Metermaid</h2>
+		</div>
+		<?php
+	}
 }
+
+add_action( 'init', array( 'METERMAID', 'init' ) );
